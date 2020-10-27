dccf467e2bd900de70518245ac7cf82c8d741c18
agBAZE <65958882+agBAZE@users.noreply.github.com>
2020-10-27 16:39:14 -0300

    Subida original
    Subida del inicio del repositorio.
    Archivos faltantes: Librerías de Unity.

---
+++ Assets/Scripts/Player/playerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-400 * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("isMoving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(400 * Time.deltaTime, 0));
+            gameObject.GetComponent<Animator>().SetBool("isMoving", true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+        if (!Input.GetKey("left") && !Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Animator>().SetBool("isMoving", false);
+        }
+    }
+}
